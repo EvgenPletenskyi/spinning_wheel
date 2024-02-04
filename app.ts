6e947064ca0b01d968cbb6f129b3fd5a197ed0c0
EvgenPletenskyi <e.pletenskyi@gmail.com>
2024-02-04 11:42:58 +0200

    some refactor

--- app.ts
+++ app.ts
@@ -1,15 +1,13 @@
 import {Application, Sprite, Container} from 'pixi.js';
 
-let winSection = 0;
+let winSection = 2;
 let rotation = false;
 let maxSpeed = 15;
-let angleOfSection = 90;
 const acceleration = 0.08;
 const stopAcceleration = 0.05;
 let rotationSpeed = 0;
-let fakeSpin = false;
 let twist = false;
-let stopAngle;
+let stopAngle: number;
 
 const w = 800;
 const h = 600;
@@ -21,7 +19,7 @@ const app = new Application<HTMLCanvasElement>({
     height: h,
 });
 globalThis.__PIXI_APP__ = app;
-// app.ticker.maxFPS = 60;
+// app.ticker.maxFPS = 20;
 
 document.getElementById("pixi_container").appendChild(app.view);
 
@@ -56,85 +54,45 @@ wheel.cursor = 'pointer';
 // wheel.on('click', wheelBehavior);
 wheel.on('click', getClick);
 app.ticker.add((delta) => {
+    resetAngle();
 
     if (rotation) {
-        rotationSpeed += acceleration * delta;
 
+        rotationSpeed += acceleration * delta;
         rotationSpeed = Math.min(rotationSpeed, maxSpeed);
         wheel.angle += rotationSpeed;
 
-        if (wheel.angle >= 360) {
-            wheel.angle = 0;
-        }
     } else if (rotationSpeed !== 0 && !rotation) {
-        //
-        // if (wheel.angle < 360 && wheel.angle !== 0 && !fakeSpin) {
-        //     wheel.angle += rotationSpeed;
-        //
-        // } else if (wheel.angle >= 360 && !fakeSpin) {
-        //     wheel.angle = 0;
-        fakeSpin = true;
-        // }
-        if (fakeSpin) {
-            // if (wheel.angle <= 360 * 4 - winSection * 180 && !twist) {
-            if (rotationSpeed !== 3 && !twist) {
-                rotationSpeed -= stopAcceleration * delta;
-                rotationSpeed = Math.max(rotationSpeed, 3);
-                wheel.angle += rotationSpeed;
-                if (wheel.angle >= 360) {
-                    wheel.angle = 0;
-                }
-            } else if (Math.abs(wheel.angle - stopAngle) >= 10 && !twist) {
-                wheel.angle += rotationSpeed;
-                if (wheel.angle >= 360) {
-                    wheel.angle = 0;
-                }
-                console.log(wheel.angle);
-                console.log(Math.abs(wheel.angle - stopAngle))
-            } else  {
-                console.log(twist);
-                twist = true;
-            }
-            if (Math.round(wheel.angle) != winSection * 90 && twist) {
-                rotationSpeed -= stopAcceleration * delta;
-                rotationSpeed = Math.max(rotationSpeed, 1);
-                wheel.angle += rotationSpeed;
-                if (wheel.angle >= 360) {
-                    wheel.angle = 0;
-                }
-            } else if (twist) {
-                rotationSpeed = 0;
-                rotation = false;
-                fakeSpin = false;
-                twist = false;
-            }
+
+        if (rotationSpeed !== 3 && !twist) {
+            rotationSpeed -= stopAcceleration * delta;
+            rotationSpeed = Math.max(rotationSpeed, 3);
+            wheel.angle += rotationSpeed;
+
+        } else if (Math.abs(wheel.angle - stopAngle) >= 10 && !twist) {
+            wheel.angle += rotationSpeed;
+
+        } else {
+            twist = true;
         }
-        //     if (wheel.angle <= 360 * 4 - winSection * 180 && fakeSpin){
-        //         console.log('wheel.angle', wheel.angle)
-        //         rotationSpeed -= stopAcceleration * delta;
-        //         rotationSpeed = Math.max(rotationSpeed, 5);
-        //         wheel.angle += rotationSpeed;
-        //     }else if (Math.round(wheel.angle) != winSection * 90) {
-        //         fakeSpin = false;
-        //         // wheel.angle = Math.floor(wheel.angle/360);
-        //         rotationSpeed -= stopAcceleration * delta;
-        //
-        //         rotationSpeed = Math.max(rotationSpeed, 1);
-        //         wheel.angle += rotationSpeed;
-        //         console.log(wheel.angle)
-        //         if (wheel.angle >= 360 - 0.001) {
-        //             wheel.angle = 0;
-        //         }
-        //
-        //     } else {
-        //         wheel.angle = winSection * 90;
-        //         rotationSpeed = 0;
-        //         console.log(wheel.angle);
-        //         rotation = false;
-        //         fakeSpin = true;
-        //     }
+        if (Math.round(wheel.angle) != winSection * 90 && twist) {
+            rotationSpeed -= stopAcceleration * delta;
+            rotationSpeed = Math.max(rotationSpeed, 1);
+            wheel.angle += rotationSpeed;
+        } else if (twist) {
+            rotationSpeed = 0;
+            rotation = false;
+            twist = false;
+        }
     }
 })
+
+function resetAngle() {
+    if (wheel.angle >= 360) {
+        wheel.angle = 0;
+    }
+}
+
 function getStopAngle(winSection) {
     switch (winSection) {
         case 0:
@@ -150,6 +108,7 @@ function getStopAngle(winSection) {
             stopAngle = 180;
     }
 }
+
 function getClick() {
     console.log('click');
     if (rotationSpeed === maxSpeed || rotationSpeed === 0) {
